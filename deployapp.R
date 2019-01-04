13f7adfaf4aa8cb97657045e41236b914aa84942
zappingseb <sebastian@mail-wolf.de>
2019-01-04 15:14:42 +0100

    Updated travis deployment

--- deployapp.R
+++ deployapp.R
@@ -1,7 +1,10 @@
 library(rsconnect)
+
 file.remove("app.R")
-file.copy(from="finished_app.R",to="finisehd_app")
-setAccountInfo(name="sebastianwolf",
-		token=Sys.getenv("shinyappstoken"),
-		secret=Sys.getenv("shinysecret"))
-deployApp()
+file.copy(from="finished_app.R",to="app.R")
+rsconnect::setAccountInfo(name="sebastianwolf",
+		token=as.character(Sys.getenv("shinyappstoken")),
+		secret=Sys.getenv("shinysecret")
+		
+		)
+deployApp(server="shinyapps.io")
